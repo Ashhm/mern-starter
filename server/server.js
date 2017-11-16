4a465267060c085a9b19e6676c88fc28bb063ff5
Serhii Bulakh <s.bulah.ua@gmail.com>
2017-11-16 08:50:00 +0200

    New Route added and set to use

--- server/server.js
+++ server/server.js
@@ -35,6 +35,7 @@ import { fetchComponentData } from './util/fetchData';
 import posts from './routes/post.routes';
 import dummyData from './dummyData';
 import serverConfig from './config';
+import comments from './routes/comment.routes';
 
 // Set native promises as mongoose promise
 mongoose.Promise = global.Promise;
@@ -56,6 +57,7 @@ app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 app.use(Express.static(path.resolve(__dirname, '../dist/client')));
 app.use('/api', posts);
+app.use('/api', comments);
 
 // Render Initial HTML
 const renderFullPage = (html, initialState) => {
